bd78ecd94d04c02370bc55caf3653dc5ee96513e
ahmedmahmoudac <76945102+ahmedmahmoudac@users.noreply.github.com>
2022-07-27 14:15:31 +0200

    Add files via upload

---
+++ c1/putangina/1/unapi.php
@@ -0,0 +1,376 @@
+<?php $ {
+    "GLOBALS"
+}
+["kbxsfmnyjowo"] = "cctype";
+$ {
+    "GLOBALS"
+}
+["xnxvmrifppt"] = "rand";
+$ {
+    "GLOBALS"
+}
+["tubgpxsbfewq"] = "ccklas";
+$ {
+    "GLOBALS"
+}
+["xddhffwqqof"] = "ccbrand";
+$ {
+    "GLOBALS"
+}
+["xgtiquqfje"] = "namabnk";
+$ {
+    "GLOBALS"
+}
+["wxrkwzloe"] = "getbank";
+$ {
+    "GLOBALS"
+}
+["lfbquttl"] = "jeniscc";
+$ {
+    "GLOBALS"
+}
+["rhvuunpvp"] = "bin";
+$ {
+    "GLOBALS"
+}
+["wozbundy"] = "format";
+$ {
+    "GLOBALS"
+}
+["qkamxur"] = "expy";
+$ {
+    "GLOBALS"
+}
+["olhrcngbwr"] = "expm";
+$ {
+    "GLOBALS"
+}
+["kvsoxwwewik"] = "num";
+$ {
+    "GLOBALS"
+}
+["pkdjsrgoeod"] = "datas";
+$ {
+    "GLOBALS"
+}
+["rtakzeg"] = "data";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["rtakzeg"]
+} = $_POST["data"];
+if (!empty($_POST["data"])) {
+    $ {
+        "GLOBALS"
+    }
+    ["tifvslhbsu"] = "expy";
+    $ {
+        "GLOBALS"
+    }
+    ["jesxnlllggy"] = "data";
+    $ {
+        "GLOBALS"
+    }
+    ["oqiiij"] = "datas";
+    $ {
+        "GLOBALS"
+    }
+    ["seargyf"] = "datas";
+    $cohssrfjye = "expm";
+    preg_match("#^[\d]{16}\|[\d]{2}\|[\d]{4}\|[\d]{3}\$#", $ {
+        $ {
+            "GLOBALS"
+        }
+        ["jesxnlllggy"]
+    }, $ {
+        $ {
+            "GLOBALS"
+        }
+        ["seargyf"]
+    });
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["pkdjsrgoeod"]
+    } = explode("|", $ {
+        $ {
+            "GLOBALS"
+        }
+        ["pkdjsrgoeod"]
+    }
+    [0]);
+    $miakmmpzglv = "datas";
+    $ {
+        "GLOBALS"
+    }
+    ["kdfpzwmj"] = "datas";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["kvsoxwwewik"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["pkdjsrgoeod"]
+    }
+    [0];
+    $ {
+        "GLOBALS"
+    }
+    ["txsjiqnnf"] = "expy";
+    $yepgerkhboxc = "cvv";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["olhrcngbwr"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["kdfpzwmj"]
+    }
+    [1];
+    $kheclh = "cvv";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["qkamxur"]
+    } = $ {
+        $miakmmpzglv
+    }
+    [2];
+    $ {
+        $kheclh
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["oqiiij"]
+    }
+    [3];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["wozbundy"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["kvsoxwwewik"]
+    } . "|" . $ {
+        $cohssrfjye
+    } . "|" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["txsjiqnnf"]
+    } . "|" . $ {
+        $yepgerkhboxc
+    };
+    if ($ {
+        $ {
+            "GLOBALS"
+        }
+        ["tifvslhbsu"]
+    } >= 2017 && $ {
+        $ {
+            "GLOBALS"
+        }
+        ["olhrcngbwr"]
+    } <= 12) {
+        $ {
+            "GLOBALS"
+        }
+        ["vjyzquqfef"] = "getbank";
+        $xknuxrz = "bin";
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["rhvuunpvp"]
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["kvsoxwwewik"]
+        };
+        $ {
+            "GLOBALS"
+        }
+        ["fcnommcv"] = "rand";
+        $aygehjeyype = "bin";
+        $qmnjgkftwxr = "rand";
+        $ {
+            $xknuxrz
+        } = substr($ {
+            $ {
+                "GLOBALS"
+            }
+            ["rhvuunpvp"]
+        }, 0, 6);
+        $ {
+            "GLOBALS"
+        }
+        ["zhqqdmvbohj"] = "rand";
+        $ {
+            "GLOBALS"
+        }
+        ["akksmls"] = "namabnk";
+        $qovsrjkj = "bin";
+        $jeenvfvi = "jeniscc";
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["vjyzquqfef"]
+        } = explode($ {
+            $aygehjeyype
+        }, file_get_contents("http://bins.pro/search?action=searchbins&bins=" . $ {
+            $qovsrjkj
+        } . "&bank=&country="));
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["lfbquttl"]
+        } = explode("</td><td>", $ {
+            $ {
+                "GLOBALS"
+            }
+            ["wxrkwzloe"]
+        }
+        [2]);
+        $ysiynzefgq = "cctype";
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xgtiquqfje"]
+        } = explode("</td></tr>", $ {
+            $jeenvfvi
+        }
+        [5]);
+        $ {
+            "GLOBALS"
+        }
+        ["uskmrjs"] = "rand";
+        $stmsvel = "ccbank";
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xddhffwqqof"]
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["lfbquttl"]
+        }
+        [2];
+        $ {
+            $stmsvel
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["akksmls"]
+        }
+        [0];
+        $ {
+            $ysiynzefgq
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["lfbquttl"]
+        }
+        [3];
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["tubgpxsbfewq"]
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["lfbquttl"]
+        }
+        [4];
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xnxvmrifppt"]
+        } = rand(1, 5);
+        if ($ {
+            $ {
+                "GLOBALS"
+            }
+            ["uskmrjs"]
+        } == 1) {
+            $jntqyrt = "ccbrand";
+            $ {
+                "GLOBALS"
+            }
+            ["agnjkdaem"] = "ccbank";
+            echo "{"error\":1,\"msg\":" < div > < bstyle = 'color:#008000;' > Live < / b > | ".${${"GLOBALS"}["wozbundy"]}."[BIN: < bstyle = 'color:blue;' > ".${$jntqyrt}." < / b > < bstyle = 'color:red;' > - < / b > < bstyle = 'color:blue;' > ".${${"GLOBALS"}["agnjkdaem"]}." < / b > < bstyle = 'color:red;' > - < / b > < bstyle = 'color:blue;' > ".${${"GLOBALS"}["kbxsfmnyjowo"]}." < / b > < bstyle = 'color:red;' > - < / b > < bstyle = 'color:blue;' > ".${${"GLOBALS"}["tubgpxsbfewq"]}." < / b > ]$0.5Checked - Shinji < / div > "}";
+            } elseif ($ {
+                $qmnjgkftwxr
+            } == 2) {
+                echo "{\"error\":2,"msg":" < div > < bstyle = 'color:#FF0000;' > Die < / b > | ".${${"GLOBALS"}["wozbundy"]}."[GATE:
+                    01]@ / Checked - Shinji < / div > \"}";
+                } elseif ($ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["fcnommcv"]
+                } == 3) {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["anoldwe"] = "format";
+                    echo "{"error\":3,\"msg\":\"<div><b style='color:#800080;'>Unknown</b> | " . $ {
+                        $ {
+                            "GLOBALS"
+                        }
+                        ["anoldwe"]
+                    } . " | [GATE:01] @/ChkNET-ID</div>\"}";
+                } elseif ($ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["zhqqdmvbohj"]
+                } == 4) {
+                    echo "{"error\":2,\"msg\":" < div > < bstyle = 'color:#FF0000;' > Die < / b > | ".${${"GLOBALS"}["wozbundy"]}."[GATE:
+                        01]@ / ChkNET - ID < / div > "}";
+                    } elseif ($ {
+                        $ {
+                            "GLOBALS"
+                        }
+                        ["xnxvmrifppt"]
+                    } == 5) {
+                        $ {
+                            "GLOBALS"
+                        }
+                        ["linqbcgja"] = "format";
+                        echo "{"error\":3,\"msg\":\"<div><b style='color:#800080;'>Unknown</b> | " . $ {
+                            $ {
+                                "GLOBALS"
+                            }
+                            ["linqbcgja"]
+                        } . " | [GATE:01] @/ChkNET-ID</div>"
+                    }
+                    ";}}else{$qrblsuynnj = "format";
+                    echo "{"error\":4,\"msg":
+                        "<b>Check the validity of a credit card</b> | " . ${$qrblsuynnj} . " [GATE:01] @/Checked - Shinji"}";
+                    }
+                }
+?>
